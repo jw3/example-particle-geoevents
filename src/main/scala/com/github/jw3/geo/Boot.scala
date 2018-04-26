30c0e7d6170f46600e7c6bb04dffdfbab38a50fb
John Wass <jwass3@gmail.com>
2018-04-26 17:08:32 -0400

    refactor configuration, clean up boot

--- src/main/scala/com/github/jw3/geo/Boot.scala
+++ src/main/scala/com/github/jw3/geo/Boot.scala
@@ -8,44 +8,40 @@ import com.typesafe.scalalogging.LazyLogging
 import net.ceedubs.ficus.Ficus._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import slick.jdbc.PostgresProfile.api._
 
-import scala.util.{Failure, Success}
-
-object Boot extends App with GeoRoutes with GeoDatabase with LazyLogging {
+object Boot extends App with BootUtils with GeoRoutes with GeoDatabase with LazyLogging {
   val config = pickConfig()
+
+  //
+  // start the system
 
   implicit val system: ActorSystem = ActorSystem("geoserver", config)
   implicit val mat: ActorMaterializer = ActorMaterializer()
 
-  val db = initdb()
-  db.foreach { db ⇒
-    import GeoConcepts.EventTable.events
-
-    val num = 10
-    val f = db.run(events.take(num).result)
-    f.onComplete {
-      case Success(r) ⇒ logger.info(s"connected to db")
-      case Failure(ex) ⇒ logger.error(s"failed to query last $num events")
-    }
-  }
-
-  val iface = "0.0.0.0"
-  val port = config.as[Int]("geo.http.port")
+  //
+  // start top level components
 
   val devices = system.actorOf(DeviceManager.props(), "devices")
   val fencing = system.actorOf(Fencer.props(), "fencing")
 
+  //
+  // start http
+
+  val iface = "0.0.0.0"
+  val port = config.as[Int]("geo.http.port")
+
   logger.info(s"starting http on $iface:$port")
   Http().bindAndHandle(routes(devices, fencing), iface, port)
+}
 
+trait BootUtils {
   def pickConfig(): Config = {
     import ConfigFactory.load
 
-    val cfg = load()
+    val cfg = load("application.conf")
     if (cfg.as[Boolean]("geo.db.ephemeral"))
-      cfg.withFallback(load("persistence-memory.conf"))
+      load("persistence-memory-app.conf")
     else
-      cfg.withFallback(load("persistence-postgres.conf"))
+      load("persistence-postgres-app.conf")
   }
 }
