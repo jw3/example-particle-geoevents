c1774c582500b33a93f3bb886bb63a9c7f8349f4
John Wass <jwass3@gmail.com>
2018-04-28 23:18:50 -0400

    start sketching in some fencing

--- src/main/scala/com/github/jw3/geo/Feature.scala
+++ src/main/scala/com/github/jw3/geo/Feature.scala
@@ -1,14 +1,23 @@
 package com.github.jw3.geo
 
-import akka.actor.{Actor, ActorLogging, Props}
-import geotrellis.vector.Geometry
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import geotrellis.vector.{Geometry, Point, Polygon}
 
 object Feature {
-  def props(id: String, geom: Geometry) = Props(new Feature(id, geom))
+  def area(name: String, geom: Polygon) = Props(new AreaFeature(name, geom))
+
+  def featureId(ref: ActorRef): String = ref.path.name
+
+  case class ContainmentQuery(geom: Point)
+  case class IntersectionQuery(geom: Geometry)
+  case class BooleanResponse(value: Boolean)
 }
 
-class Feature(id: String, geom: Geometry) extends Actor with ActorLogging {
+class AreaFeature(name: String, geom: Polygon) extends Actor with ActorLogging {
   def receive: Receive = {
-    case _ ⇒
+    case Feature.ContainmentQuery(pt) ⇒
+      sender ! Feature.BooleanResponse(geom.contains(pt))
+    case Feature.IntersectionQuery(g) ⇒
+      sender ! Feature.BooleanResponse(geom.intersects(g))
   }
 }
--- src/main/scala/com/github/jw3/geo/Fencer.scala
+++ src/main/scala/com/github/jw3/geo/Fencer.scala
@@ -1,24 +1,30 @@
 package com.github.jw3.geo
 
+import java.util.UUID
+
 import akka.actor.{Actor, ActorLogging, Props}
 import com.github.jw3.geo.Fencer.{FeatureCreated, FromGeoJson}
-import geotrellis.vector.Geometry
 import geotrellis.vector.io.json.GeoJson
 import geotrellis.vector.io.json.Implicits._
+import geotrellis.vector.{Geometry, Polygon}
 
 object Fencer {
   def props() = Props(new Fencer)
 
-  final case class FromGeoJson(rid: String, json: String)
-  final case class FeatureCreated(rid: String, fid: String)
+  final case class FromGeoJson(name: String, json: String)
+  final case class FeatureCreated(name: String, fid: String)
 }
 
 class Fencer extends Actor with ActorLogging {
   def receive: Receive = {
-    case FromGeoJson(rid, json) ⇒
+    case FromGeoJson(name, json) ⇒
       val geom = GeoJson.parse[Geometry](json)
-      val fid = "some_generated_id"
-      context.actorOf(Feature.props(fid, geom))
-      sender ! FeatureCreated(rid, fid)
+      val fid = UUID.randomUUID.toString.take(8)
+
+      geom match {
+        case g @ Polygon(_) ⇒
+          context.actorOf(Feature.area(name, g), fid)
+          sender ! FeatureCreated(name, fid)
+      }
   }
 }
