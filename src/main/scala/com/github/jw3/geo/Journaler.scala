7182e7f613e17781c7df7b74b185b76878f75e97
John Wass <jwass3@gmail.com>
2018-05-10 23:36:36 -0400

    read-side offset tracking

--- src/main/scala/com/github/jw3/geo/Journaler.scala
+++ src/main/scala/com/github/jw3/geo/Journaler.scala
@@ -3,11 +3,15 @@ package com.github.jw3.geo
 import akka.actor.{Actor, ActorLogging, Props}
 import akka.persistence.query.{EventEnvelope, Offset}
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Sink
 import com.github.jw3.geo.Api.Events.{PositionUpdate, TrackCompleted}
+import com.github.jw3.geo.Api.Tags
 import com.github.jw3.geo.GeoConcepts.EventTable.events
 import com.github.jw3.geo.GeoConcepts.TrackTable.tracks
+import com.github.jw3.geo.Journaler.{MovePersisted, TrackPersisted}
+import com.github.jw3.geo.OffsetStore.updateOffsetQuery
 import com.github.jw3.geo.PgDriver.api._
-import geotrellis.vector.Line
+import geotrellis.vector.{Line, Point}
 
 import scala.util.{Failure, Success}
 
@@ -16,45 +20,57 @@ import scala.util.{Failure, Success}
   */
 object Journaler {
   def props(db: Database)(implicit mat: ActorMaterializer) = Props(new Journaler(db))
+  final val Id = "journaler"
+
+  case class TrackPersisted(id: String)
+  case class MovePersisted(device: String, pos: Point)
 }
 
 class Journaler(db: Database)(implicit mat: ActorMaterializer) extends Actor with ActorLogging {
   import context.{dispatcher, system}
 
-  Streams.movement().runForeach {
-    case EventEnvelope(_, _, _, PositionUpdate(dev, pos)) ⇒
-      val action = events += ((0, dev, pos))
-      db.run(action).onComplete(context.self ! _)
-    case _ ⇒
+  db.run(OffsetStore.getOffsetQuery(Journaler.Id, Tags.Movement)).flatMap { start ⇒
+    // moves to child actor, MoveJournaler
+    Streams.movement(start).runForeach {
+      case EventEnvelope(offset, _, _, PositionUpdate(dev, pos)) ⇒
+        val action = (events += ((0, dev, pos))).flatMap { _ ⇒
+          updateOffsetQuery(Journaler.Id, Tags.Movement, offset)
+        }
+        db.run(action.transactionally).onComplete(_ ⇒ context.self ! Journaler.MovePersisted(dev, pos))
+
+      case _ ⇒
+    }
   }
 
-  Streams.tracking().runForeach {
-    case EventEnvelope(_, _, _, TrackCompleted(id, dev, begin, end, pt1, pt2)) ⇒
-      log.debug("completed track {} at {}", id, pt2)
+  db.run(OffsetStore.getOffsetQuery(Journaler.Id, Tags.Tracks)).flatMap { start ⇒
+    // moves to child actor, TrackJournaler
+    Streams.tracking(start).runForeach {
+      case EventEnvelope(offset, _, _, TrackCompleted(id, dev, begin, end, pt1, pt2)) ⇒
+        log.info("completed track {} at {}", id, pt2)
 
-      Streams
-        .movement(Offset.sequence(begin))
-        .takeWhile(_.sequenceNr != end)
-        .map(_.event)
-        .filter {
-          case _: PositionUpdate ⇒ true
-          case _ ⇒ false
-        }
-        .map(_.asInstanceOf[PositionUpdate])
-        .fold(Line(pt1, pt2)) { (line, mv) ⇒
-          Line(line.points.dropRight(1) :+ mv.pos :+ line.points.last: _*)
-        }
-        .runForeach { line ⇒
-          log.debug("saving track for {} as {}", dev, line)
-          val action = tracks += ((0, dev, begin, line))
-          db.run(action).onComplete(context.self ! _)
-        }
+        Streams
+          .movement(Offset.sequence(begin))
+          .takeWhile(_.sequenceNr != end)
+          .map(_.event)
+          .filter(_.isInstanceOf[PositionUpdate])
+          .map(_.asInstanceOf[PositionUpdate])
+          .fold(Line(pt1, pt2)) { (line, mv) ⇒
+            Line(line.points.dropRight(1) :+ mv.pos :+ line.points.last: _*)
+          }
+          .log("journaler", line ⇒ s"saving track for $dev as $line")
+          .map(line ⇒ tracks += ((0, dev, begin, line)))
+          .map(action ⇒ action.flatMap(_ ⇒ updateOffsetQuery(Journaler.Id, Tags.Tracks, offset)))
+          .mapAsync(1)(action ⇒ db.run(action.transactionally))
+          .runWith(Sink.actorRef(self, TrackPersisted(id)))
 
-    case _ ⇒
+      case _ ⇒
+    }
   }
 
   def receive: Receive = {
     case Success(_) ⇒ log.debug("read-side journal success")
     case Failure(ex) ⇒ log.error(ex, "read-side journal failure")
+    case TrackPersisted(id) ⇒ log.debug("persisted track {}", id)
+    case MovePersisted(dev, pt) ⇒ log.debug("persisted move {} to {}", dev, pt)
   }
 }
